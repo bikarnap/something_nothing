0bf50c512dc6063af2f6d9e055d67f8cc834884f
Bikarna Pokharel <bikarnap@gmail.com>
2023-03-31 12:04:44 +0000

    Add tests

--- Reservation.java
+++ Reservation.java
@@ -1,33 +1,79 @@
 import java.util.Date;
 
+/**
+ * @author  Bikarna Pokharel &lt;bikarnap@gmail.com&gt;
+ * @version 2023.0325
+ * @since 17.0
+ */
+/**
+ * This class is responsible for a Reservation object.
+ */
 public class Reservation {
+  /**
+   * Declares a User object user.
+   * <p> It is to hold the user of reservation.
+   */
   private User user;
+  /**
+   * Declares a Book object book.
+   * <p> It is to hold the book that is reserved.
+   */
   private Book book;
+  /**
+   * Declaes a Date object date.
+   * <p> It is to hold the date of reservation.
+   */
   private Date date;
 
-
+  /**
+   * Constructs a Reservation object with the provided arguments.
+   * @param user the user who has made reservation
+   * @param book the book that is reserved
+   * @param date the data of reservation
+   */
   public Reservation(User user, Book book, Date date) {
     this.user = user;
     this.book = book;
     this.date = date;
   }
 
+  /**
+   * Gets the user who has rserved a book.
+   * @return the user
+   */
   public User getUser() {
     return user;
   }
 
+  /**
+   * Sets the user who has reserved the book.
+   * @param user the user 
+   */
   public void setUser(User user) {
     this.user = user;
   }
 
+  /**
+   * Gets the book that is reserved.
+   * @return the reserved book
+   */
   public Book getBook() {
     return book;
   }
 
+  /**
+   * Gets the date of reservatoin.
+   * @return data of reservation
+   */
   public Date getDate() {
     return date;
   }
 
+  /**
+   * Override method to display information about the reservation.
+   * Overrides toString method.
+   * @return the string of information
+   */
   @Override
   public String toString() {
     return "Reservation for [user=" + user + ", book=" + book + ", date=" + date + "]";
--- ReservationTest.java
+++ ReservationTest.java
@@ -1,5 +1,6 @@
 import static org.junit.Assert.assertEquals;
 
+import java.util.Calendar;
 import java.util.Date;
 
 import org.junit.Before;
@@ -33,38 +34,161 @@ public class ReservationTest {
     reservation3 = new Reservation(user3, book3, date3);
   }
 
+  /**
+   * Tests the getBook method.
+   * <p> Uses the title of the book for checking.
+   */
   @Test
   public void testGetBook1() {
+    book1 = new Book("Advanced Software Testing Vol. 1", 
+    "Black R.", "1912");
     assertEquals("Advanced Software Testing Vol. 1", book1.getTitle());
   }
 
+  /**
+   * Tests the getBook method.
+   * <p> Uses the title of the book for checking.
+   */
   @Test
   public void testGetBook2() {
+    book2 = new Book("The Software Engineer's Handbook, 1st editonAdvanced Software Testing Vol. 1", 
+      "Graham B & McKay J", "1911");
     assertEquals("The Software Engineer's Handbook, 1st editonAdvanced Software Testing Vol. 1", book2.getTitle());
   }
 
+  /**
+   * Tests the getBook method.
+   * <p> Uses the title of the book for checking.
+   */
   @Test
   public void testGetBook3() {
+    book3 = new Book("Software Testing, 2nd edition", 
+      "Patton R", "1946");
     assertEquals("Software Testing, 2nd edition", book3.getTitle());
   }
 
+  /**
+   * Test the getDate method.
+   * <p> Checks if the reservation date passes the test.
+   */
   @Test
   public void testGetDate1() {
-    assertEquals(date1, new Date());
+    date1 = new Date();
+    reservation1 = new Reservation(user1, book1, date1);
+    assertEquals(date1, reservation1.getDate());
   }
 
-  // @Test
-  // public void testGetUser() {
+  /**
+   * Test the getDate method.
+   * <p> Checks if the reservation date passes the test.
+   */
+  @Test
+  public void testGetDate2() {
+    Date now = new Date();
+    Calendar calendar = Calendar.getInstance();
+    calendar.setTime(now);
+    calendar.add(Calendar.DATE, 14);
+    date2 = calendar.getTime();
+    reservation2 = new Reservation(user1, book1, date2);
+    assertEquals(date2, reservation2.getDate());
+  }
 
-  // }
+  /**
+   * Test the getDate method.
+   * <p> Checks if the reservation date passes the test.
+   */
+  @Test
+  public void testGetDate3() {
+    Date now = new Date();
+    Calendar calendar = Calendar.getInstance();
+    calendar.setTime(now);
+    calendar.add(Calendar.DATE, -4);
+    date3 = calendar.getTime();
+    reservation3 = new Reservation(user1, book1, date3);
+    assertEquals(date3, reservation3.getDate());
+  }
 
-  // @Test
-  // public void testSetUser() {
+  /**
+   * Tests the getUser method.
+   * <p> Uses assertEquals to check.
+   */
+  @Test
+  public void testGetUser1() {
+    assertEquals(user1, reservation1.getUser());
+  }
 
-  // }
+  /**
+   * Tests the getUser method.
+   * <p> Uses assertEquals to check.
+   */
+  @Test
+  public void testGetUser2() {
+    assertEquals(user2, reservation2.getUser());
+  }
 
-  // @Test
-  // public void testToString() {
+  /**
+   * Tests the getUser method.
+   * <p> Uses assertEquals to check.
+   */
+  @Test
+  public void testGetUser3() {
+    assertEquals(user3, reservation3.getUser());
+  }
 
-  // }
+  /**
+   * Tests the setUser method.
+   * <p> Uses assertEquals to check.
+   */
+  @Test
+  public void testSetUser1() {
+    User user4 = new User("Steve Jobs", "+123456789", 24);
+    reservation1.setUser(user4);
+    assertEquals(user4, reservation1.getUser());
+  }
+
+  /**
+   * Tests the setUser method.
+   * <p> Uses assertEquals to check.
+   */
+  @Test
+  public void testSetUser2() {
+    User user5 = new User("Steve Jobs", "+123456789", 24);
+    reservation2.setUser(user5);
+    assertEquals(user5, reservation2.getUser());
+  }
+
+  /**
+   * Tests the setUser method.
+   * <p> Uses assertEquals to check.
+   */
+  @Test
+  public void testSetUser3() {
+    User user6 = new User("Steve Jobs", "+123456789", 24);
+    reservation3.setUser(user6);
+    assertEquals(user6, reservation3.getUser());
+  }
+
+  /**
+   * Tests the toString method.
+   */
+  @Test
+  public void testToString1() {
+    assertEquals("Reservation for [user=" + user1 + ", book=" + book1 + ", date=" + date1 + "]", reservation1.toString());
+  }
+
+  /**
+   * Tests the toString method.
+   */
+  @Test
+  public void testToString2() {
+    assertEquals("Reservation for [user=" + user2 + ", book=" + book2 + ", date=" + date2 + "]", reservation2.toString());
+  }
+
+  /**
+   * Tests the toString method.
+   */
+  @Test
+  public void testToString3() {
+    assertEquals("Reservation for [user=" + user3 + ", book=" + book3 + ", date=" + date3 + "]", reservation3.toString());
+  }
 }
